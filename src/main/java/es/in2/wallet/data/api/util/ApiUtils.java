5ff07a583e5af8a5e2f2275d2f6d6537317dc2c4
Ruben Modamio Garcia <ruben.modamio@in2.es>
2023-12-05 11:48:04 +0100

    added log

--- src/main/java/es/in2/wallet/data/api/util/ApiUtils.java
+++ src/main/java/es/in2/wallet/data/api/util/ApiUtils.java
@@ -11,7 +11,7 @@ public final class ApiUtils {
     public static final String CREDENTIAL_SUBJECT = "credentialSubject";
 
     public static final String GLOBAL_ENDPOINTS_API = "/api/v1/*";
-    public static final String ALLOWED_METHODS = "/api/v1/*";
+    public static final String ALLOWED_METHODS = "*";
     // Private constructor to prevent instantiation
     private ApiUtils() {}
 
